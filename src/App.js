76266433095d23a62c8692dcef8eeb08b63ce2de
yakreb <berkayturna@gmail.com>
2025-02-26 23:46:49 +0100

    Add form handling for adding friends with name and image URL

--- src/App.js
+++ src/App.js
@@ -65,11 +65,27 @@ function Button({ children, onClick }) {
   return <button className="button" onClick={onClick}>{children}</button>
 }
 function FormAddFriend() {
-  return <form className="form-add-friend">
+  const [name, setName] = useState("")
+  const [image, setImage] = useState("https://i.pravatar.cc/48")
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID()
+    const newFriend = {
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+      id
+    }
+  }
+  return <form className="form-add-friend" onSubmit={handleSubmit}>
     <label>🤸‍♀️Friend name</label>
-    <input type="text" />
+    <input type="text" value={name} onChange={event => setName(event.target.value)} />
     <label>🌅Image URL</label>
-    <input type="text" />
+    <input type="text" value={image} onChange={event => setImage(event.target.value)} />
     <Button>Add</Button>
   </form>
 }
